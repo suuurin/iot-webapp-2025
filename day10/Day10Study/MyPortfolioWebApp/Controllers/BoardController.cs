8a0737dcb33e3fae53022eba651243581ca63c73
suuurin <z11673@naver.com>
2025-06-09 18:04:52 +0900

    11일차 커밋

--- day10/Day10Study/MyPortfolioWebApp/Controllers/BoardController.cs
+++ day10/Day10Study/MyPortfolioWebApp/Controllers/BoardController.cs
@@ -19,10 +19,56 @@ namespace MyPortfolioWebApp.Controllers
         }
 
         // GET: Board
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int page = 1, string search = "")
         {
-            
-            return View(await _context.Board.ToListAsync());
+            // 뷰쪽에 보내고 싶은 데이터
+            //ViewData["Key"] //ViewBag.Title //TempData["Key"]
+            ViewData["Title"] = "서버에서 변경가능";
+            // _context News
+            // SELECT * FROM News.
+            //return View(await _context.News
+            //                       .OrderByDescending(o => o.PostDate)
+            //                       .ToListAsync()); // 뷰화면에 데이터를 가지고감
+            // 쿼리로 처리가능
+            //var news = await _context.News.FromSql($@"SELECT Id, Writer, Title, Description, PostDate, ReadCount
+            //                                            FROM News")
+            //                        .OrderByDescending(o => o.PostDate)
+            //                        .ToListAsync();
+            //return View(news);
+
+            // 최종단계
+            // 페이지 개수
+            //var totalCount = _context.News.FromSql($@"SELECT * FROM News WHERE Title LIKE '%{search}%'").Count(); // 현재 문제발생!!
+            var totalCount = _context.Board.Where(n => EF.Functions.Like(n.Title, $"%{search}%")).Count(); // HACK! 위 구문이 오류발생
+            var countList = 10; // 한페이지에 기본 뉴스갯수 10개
+            var totalPage = totalCount / countList; // 한페이지당 개수로 나누면 전체페이지 수
+            // HACK : 게시판페이지 중요로직. 남는 데이터도 한페이지를 차지해야 함
+            if (totalCount % countList > 0) totalPage++;  // 남은 게시글이 있으면 페이지수 증가            
+            if (totalPage < page) page = totalPage;
+            // 마지막 페이지 구하기
+            var countPage = 10; // 페이지를 표시할 최대페이지개수, 10개
+            var startPage = ((page - 1) / countPage) * countPage + 1;
+            var endPage = startPage + countPage - 1;
+            // HACK : 나타낼 페이수가 10이 안되면 페이지수 조정.
+            // 마지막페이지까지 글이 12개면  1, 2 페이지만 표시
+            if (totalPage < endPage) endPage = totalPage;
+
+            // 저장프로시저에 보낼 rowNum값, 시작번호랑 끝번호
+            var startCount = ((page - 1) * countPage) + 1; // 2페이지의 경우 11
+            var endCount = startCount + countList - 1; // 2페이지의 경우 20
+
+            // View로 넘기는 데이터, 페이징 숫자컨트롤 사용
+            ViewBag.StartPage = startPage;
+            ViewBag.EndPage = endPage;
+            ViewBag.Page = page;
+            ViewBag.TotalPage = totalPage;
+            ViewBag.Search = search; // 검색어
+
+            // 저장프로시저 호출
+            var board = await _context.Board
+            .FromSqlInterpolated($"CALL Free_PagingBoard({startCount}, {endCount}, {search})")
+            .ToListAsync();
+            return View(board);
         }
 
         // GET: Board/Details/5
@@ -40,28 +86,50 @@ namespace MyPortfolioWebApp.Controllers
                 return NotFound();
             }
 
+            // 조회수 증가 로직
+            board.ReadCount++;
+            _context.Board.Update(board);
+            await _context.SaveChangesAsync();
+
             return View(board);
         }
 
         // GET: Board/Create
         public IActionResult Create()
         {
-            return View();
+            var board = new Board
+            {
+                Writer = User.Identity?.Name ?? "익명",
+                Email = "unknown@example.com", // Email 추후 개선 가능
+                PostDate = DateTime.Now,
+                ReadCount = 0
+            };
+            return View(board);
         }
+
+
 
         // POST: Board/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Email,Writer,Title,Contents,PostDate,ReadCount")] Board board)
+        public async Task<IActionResult> Create([Bind("Id,Title,Contents")] Board board)
         {
             if (ModelState.IsValid)
             {
+                board.Writer = User.Identity?.Name ?? "익명";
+                board.Email = "unknown@example.com";
+                board.PostDate = DateTime.Now;
+                board.ReadCount = 0;
+
                 _context.Add(board);
                 await _context.SaveChangesAsync();
+
+                TempData["success"] = "게시글 작성 완료!";
                 return RedirectToAction(nameof(Index));
             }
+
             return View(board);
         }
 
@@ -86,7 +154,7 @@ namespace MyPortfolioWebApp.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Email,Writer,Title,Contents,PostDate,ReadCount")] Board board)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Contents")] Board board)
         {
             if (id != board.Id)
             {
@@ -97,7 +165,16 @@ namespace MyPortfolioWebApp.Controllers
             {
                 try
                 {
-                    _context.Update(board);
+                    // 원래 글을 DB에서 불러옴
+                    var existingBoard = await _context.Board.FindAsync(id);
+                    if (existingBoard == null)
+                        return NotFound();
+
+                    // 제목과 내용만 수정
+                    existingBoard.Title = board.Title;
+                    existingBoard.Contents = board.Contents;
+
+                    // 나머지 필드 (작성자, 이메일, 날짜, 조회수 등)는 유지
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
@@ -146,6 +223,8 @@ namespace MyPortfolioWebApp.Controllers
             }
 
             await _context.SaveChangesAsync();
+
+            TempData["success"] = "뉴스 삭제 성공!";
             return RedirectToAction(nameof(Index));
         }
 
